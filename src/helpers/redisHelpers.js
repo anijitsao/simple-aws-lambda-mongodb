2ff7cb54b229d8164579f3e3740085b9ae6babcc
anijit <anijitsau@gmail.com>
2022-07-01 18:58:38 +0530

    redis helpers are written

--- src/helpers/redisHelpers.js
+++ src/helpers/redisHelpers.js
@@ -1,5 +1,19 @@
+import { redisClient } from "../lib/redisClient.js";
+
 const connectRedis = async () => {
-  await client.connect();
+  await redisClient.connect();
 };
 
-export { connectRedis };
+// get value based on the key provided
+const getValue = async (key) => {
+  const value = await redisClient.get(key);
+  return value;
+};
+
+// set the value to key provided
+const setValue = async (key, value) => {
+  await redisClient.set(key, value);
+  return;
+};
+
+export { connectRedis, getValue, setValue };
--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,8 +1,14 @@
-// local dep
+// local dependencies
+import { setValue, getValue, connectRedis } from "../helpers/redisHelpers.js";
 import { sendResponse } from "../helpers/sendResponse.js";
+
 const checkRedisHandler = async (event) => {
-  const res = { message: "API endpoint reached" };
-  sendResponse(process.env.SUCCESS_CODE, res);
+  try {
+    const res = { message: "API endpoint reached" };
+    return sendResponse(process.env.SUCCESS_CODE, res);
+  } catch (error) {
+    return sendResponse(process.env.ERROR_CODE, { error: error.toString() });
+  }
 };
 
 export { checkRedisHandler };
